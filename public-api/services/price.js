ac1a9a3815a2f8b8fe864d78aacf455158dc0790
Aaditya-Nandan <aadityanandan05@gmail.com>
2023-02-07 21:54:22 +0530

    refactored price.js

--- public-api/services/price.js
+++ public-api/services/price.js
@@ -12,25 +12,6 @@ class Price extends BaseService {
     "method": "GET"
   };
 
-  // async priceHistory() {
-  //   const { stockSymbol } = this.params;
-  //   const from = moment()
-  //     .subtract(2, 'years')
-  //     .subtract(1, 'day')
-  //     .format('DD-MM-YYYY');
-  //   const to = moment().format('DD-MM-YYYY');
-  //   const url = `https://www.nseindia.com/api/historical/cm/equity?symbol=${stockSymbol}&series=[%22EQ%22]&from=${from}&to=${to}&csv=true`;
-  //   let parsedData = [];
-  //   try {
-  //     parsedData = await parseCSVFromURL(url);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  //   return {
-  //     data: parsedData,
-  //   };
-  // }
-
   async  populateStockDetails() {
     const session = await axios.get('https://www.nseindia.com');
     this.config.headers.cookie = session.headers['set-cookie'].join(";");
