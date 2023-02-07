2eecb0e7ac982d6e57159cfdbdf825154050d573
Adhyan Gupta <gadhyan250@gmail.com>
2023-02-07 21:45:55 +0530

    feat:moved price routes to services

--- public-api/routes/prices.js
+++ public-api/routes/prices.js
@@ -1,13 +1,10 @@
 const axios = require('axios');
 const express = require('express');
 const responseHandler = require('../../toolbox/helpers/response-handler');
+const Price = require('../services/price');
 const router = express.Router();
 const {PriceTicksModel} = require('../models');
-const config = {
-    "headers": {
-    },
-    "method": "GET"
-  };
+
 
 
 function buildDefaultPriceResponse(stockSymbol){
@@ -53,21 +50,11 @@ function buildDefaultPriceResponse(stockSymbol){
     };
 }
 
-async function populateStockDetails() {
-    const session = await axios.get('https://www.nseindia.com');
-    config.headers.cookie = session.headers['set-cookie'].join(";");
-    try {
-        const data = await axios('https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%20TOTAL%20MARKET',config);
-        await PriceTicksModel.deleteMany({});
-        await PriceTicksModel.insertMany(data.data.data);
-        return{d: 'Done'};
-    } catch (error) {
-        console.log(error);
-    }
-};
+
 
 router.put('/', (req,res)=>{
-    return responseHandler(populateStockDetails(),res);
+    const priceService = new Price(req);
+    return responseHandler(priceService.populateStockDetails(),res);
 });
 
 router.get('/', async (req,res)=>{
