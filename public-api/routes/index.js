39db92e6fba727486fd4dc672322cb8d43d913ca
Vasav Prashar <Vasav.420@gmail.com>
2023-02-13 22:27:31 +0530

    feat: added news api

--- public-api/routes/index.js
+++ public-api/routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphRouter = require('./graph');
 const stocksRouter = require('./stocks');
 const pricesRouter = require('./prices');
+const newsRouter = require('./news');
+
 const router = express.Router();
 router.use('/graph', graphRouter);
 router.use('/stocks',stocksRouter);
 router.use('/prices',pricesRouter);
+router.use('/news',newsRouter);
 module.exports = router;
