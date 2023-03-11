216227c0182f6d2df1f0e525a509c645d63a0cc2
parth0105pluang <98079965+parth0105pluang@users.noreply.github.com>
2023-03-11 23:23:33 +0530

    fix: amount concat issue

---
+++ public-api/services/leader-board.js
@@ -0,0 +1,14 @@
+const BaseService = require('./base-service');
+const { BalanceModel } = require('../models')
+class Wallet extends BaseService {
+  constructor(props) {
+    super(props);
+  }
+
+  async getUserBalance() {
+    const { userId } = this.params;
+    const userBalance = await BalanceModel.findOne({userId})
+    return {data: userBalance };
+  }
+}
+module.exports = Wallet;
