b45d43e2cf401c1e11287c63eac18624259979de
parth0105pluang <98079965+parth0105pluang@users.noreply.github.com>
2023-04-01 17:49:46 +0530

    fix: graph api

--- public-api/services/graph.js
+++ public-api/services/graph.js
@@ -9,7 +9,7 @@ class Graph extends BaseService {
   }
 
   async fetchHistoricalDataFromNSE(stockSymbol){
-    const maxDays = 750;
+    const maxDays = 721;
     const from = moment()
       .subtract(maxDays, 'days')
       .subtract(1, 'day')
@@ -26,14 +26,14 @@ class Graph extends BaseService {
   }
 
   async priceHistory() {
-      const { stockSymbol, timeFrame } = this.query;
+      const { stockSymbol, timeframe } = this.query;
       const firebaseAccessors = new FirebaseAccessors();
       let data = await firebaseAccessors.getHistoricalData(stockSymbol);
       if(!data){
-        data = await this.fetchHistoricalDataFromNSE();
+        data = await this.fetchHistoricalDataFromNSE(stockSymbol);
         await firebaseAccessors.setHistoricalData(stockSymbol,data);
       }
-      return { data: data.slice(0,timeFrame) };
+      return { data: data.slice(0,timeframe) };
   }
 }
 module.exports = Graph;
