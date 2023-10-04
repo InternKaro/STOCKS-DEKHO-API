a0319ba0e69034bdd6450e8ef534aa8c656c439c
parth0105pluang <98079965+parth0105pluang@users.noreply.github.com>
2023-10-04 22:55:10 +0530

    Update requestValidator.js

--- toolbox/requestValidator.js
+++ toolbox/requestValidator.js
@@ -3,10 +3,10 @@ const validateRequest = (schema,reqPart) =>
     (req,res,next) =>{
         const {value,error} = schema.validate(req[reqPart],{abortEarly: false});
         if(error){
-            res.status(422).json({message: error.message})
+            res.status(422).json({message: error.message});
         }
         else{
             next();
         }
     }
-module.exports = {validateRequest};
+module.exports = { validateRequest };
